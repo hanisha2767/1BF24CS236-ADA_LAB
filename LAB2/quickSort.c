d73e114fb50ed663f9a2f7dc0e816b00e69c6950
hanisha2767 <hanishareddy.cs24@bmsce.ac.in>
2026-04-01 15:37:48 +0530

    Add files via upload

---
+++ LAB2/quickSort.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int n,arr[100000];
+    clock_t start,end;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    srand(time(0));
+
+    printf("Random numbers generated\n");
+    for (int i = 0; i < n; i++) {
+        arr[i]=rand()% 100000;
+        printf("%d ",arr[i]);
+    }
+
+    start = clock();
+    quickSort(arr, 0, n - 1);
+    end = clock();
+
+    printf("\nSorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("\nTime taken: %f seconds\n", time_taken);
+
+    return 0;
+}
