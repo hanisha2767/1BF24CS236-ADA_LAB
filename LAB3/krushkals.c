b79321a1e99dfb329690d64edc82a4a6c163e926
hanisha2767 <hanishareddy.cs24@bmsce.ac.in>
2026-06-08 22:29:39 +0530

    Update krushkals.c

--- LAB3/krushkals.c
+++ LAB3/krushkals.c
@@ -26,22 +26,6 @@ int main() {
         scanf("%d %d %d", &u[i], &v[i], &w[i]);
 
     for(i = 0; i < e - 1; i++) {
-        for(j = 0; j < e - i - 1; j++) {
-            if(w[j] > w[j+1]) {
-                int temp;
-
-                temp = w[j]; w[j] = w[j+1]; w[j+1] = temp;
-                temp = u[j]; u[j] = u[j+1]; u[j+1] = temp;
-                temp = v[j]; v[j] = v[j+1]; v[j+1] = temp;
-            }
-        }
-    }
-
-    int mincost = 0, count = 0;
-
-    printf("\nEdges in MST:\n");
-
-    for(i = 0; i < e - 1; i++) {
         for(j = i+1; j < e ; j++) {
             if(w[i] > w[j]) {
                 int temp;
@@ -50,6 +34,22 @@ int main() {
                 temp = u[i]; u[i] = u[j]; u[j] = temp;
                 temp = v[i]; v[i] = v[j]; v[j] = temp;
             }
+        }
+    }
+
+    int mincost = 0, count = 0;
+
+    printf("\nEdges in MST:\n");
+
+    for(i = 0; i < e && count < n - 1; i++) {
+        int pu = find(u[i]);
+        int pv = find(v[i]);
+
+        if(pu != pv) {
+            printf("%d -> %d = %d\n", u[i], v[i], w[i]);
+            mincost += w[i];
+            unionSet(pu, pv);
+            count++;
         }
     }
 
